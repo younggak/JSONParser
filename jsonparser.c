17ba34bc681febaf5d8a78a2fef474e6f7f34b16
OSS_kjlee <21400506@handong.edu>
2019-05-16 17:37:16 +0900

    second commit

--- jsonparser.c
+++ jsonparser.c
@@ -1,2 +1,22 @@
-int main() {
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	char file_name[strlen(argv[1])];
+	strcpy(file_name, argv[1]);
+	for(int i = 0; i < strlen(file_name); i++) {
+		printf("%c", file_name[i]);
+	}
+	int dot_index = 0;
+	for(int i = 0; i < strlen(file_name); i++)
+		if(file_name[i] == '.') 
+			dot_index = i;
+	char file_type[strlen(file_name)-dot_index];
+	strcpy(file_type, &file_name[dot_index+1]);	
+	if(strcmp(file_type, "json") != 0)
+		exit(0);
+	// read file
+	//
+	printf("it is jason\n");
 }
