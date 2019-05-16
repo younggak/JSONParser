0f691c86f3cfad5f1b4688b0275c49abe61d8f29
OSS_kjlee <21400506@handong.edu>
2019-05-16 17:57:38 +0900

    second commit

--- jsonparser.c
+++ jsonparser.c
@@ -5,9 +5,7 @@
 int main(int argc, char **argv) {
 	char file_name[strlen(argv[1])];
 	strcpy(file_name, argv[1]);
-	for(int i = 0; i < strlen(file_name); i++) {
-		printf("%c", file_name[i]);
-	}
+
 	int dot_index = 0;
 	for(int i = 0; i < strlen(file_name); i++)
 		if(file_name[i] == '.') 
@@ -16,7 +14,18 @@ int main(int argc, char **argv) {
 	strcpy(file_type, &file_name[dot_index+1]);	
 	if(strcmp(file_type, "json") != 0)
 		exit(0);
-	// read file
-	//
-	printf("it is jason\n");
+
+	FILE *fp;
+	fp = fopen(file_name, "r");
+	if(fp == NULL) {
+		printf("File doesn't exist!\n");
+	}
+	printf("File is openned.\nFile contains the following: \n");
+
+	int c;
+	do {
+		c = fgetc(fp);
+		printf("%c", c);
+	} while(c != EOF);
+	fclose(fp);
 }
