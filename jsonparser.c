917c3fb4b825be8f2f3226465ce0c63fbc871333
OSS_kjlee <21400506@handong.edu>
2019-05-27 17:45:44 +0900

    20190527

--- jsonparser.c
+++ jsonparser.c
@@ -19,8 +19,8 @@ typedef struct {
 } jsmntok_t;
 
 int tokens(char *buffer);
-void print(jsmntok_t*token,int num,char*buffer);
-void tokenizer(jsmntok_t *token,char *buffer, int index);
+void print(jsmntok_t*token, int num, char*buffer);
+void tokenizer(jsmntok_t *token, char *buffer, int index);
 
 int main(int argc, char **argv) {
 	char file_name[strlen(argv[1])];
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
 	if(fp == NULL) {
 		printf("File doesn't exist!\n");
 	}
-	printf("File is openned.\nFile contains the following: \n");
+	printf("***** ALL TOKENS *****\n");
 
 	char c;
 	char *buffer = (char*)malloc(sizeof(char) * 1024);
@@ -54,65 +54,80 @@ int main(int argc, char **argv) {
 	
 	jsmntok_t *token = (jsmntok_t *)malloc(sizeof(jsmntok_t) * tokens(buffer));
 
-	tokenizer(token,buffer,index);
-	print(token,tokens(buffer),buffer);
+	tokenizer(token, buffer, index);
+	print(token, tokens(buffer), buffer);
 }
 
-void tokenizer(jsmntok_t *token, char *buffer, int index){
-	int token_num=0;
-	int temp,end=0;
-	int obj=0,arr=0,str=0,prm=0;
-	for(int i=0;i<index;i++){
-		if(buffer[i]=='{'){
-			token[token_num].type=OBJECT;
-			token[token_num].start=i;
-			temp=obj;
-			for(end=i+1;;end++){
-				if(buffer[end]=='{')
+void tokenizer(jsmntok_t *token, char *buffer, int len){
+	int token_num = 0;
+	int temp, end, size = 0;
+	int obj = 0, arr = 0, str = 0, prm = 0;
+
+	for(int i = 0; i < len; i++) {
+		if(buffer[i] == '{') {
+			token[token_num].type = OBJECT;
+			token[token_num].start = i;
+			temp = obj;
+			for(end = i+1; ; end++){
+				if(buffer[end] == '{')
 					temp++;
-				if(buffer[end]=='}')
+				if(buffer[end] == '}')
 					temp--;
-				if(temp<0)
+				if(temp < 0)
 					break;
 			}
-			token[token_num].end=end;
-			if(token_num==0)
-				token[token_num].end--;
+			token[token_num].end = end;
 			obj++;
-			token_num++;
+			size=0;
+			for(int j = i; j < end; j++) {
+				if(buffer[j] == ':') size++;
+			}
+			token[token_num].size = size;
+			token_num++; // why?
 		}
-		if(buffer[i]=='['){
-			token[token_num].type=ARRAY;
-			token[token_num].start=i;
-			temp=arr;
-			for(end=i+1;;end++){
-				if(buffer[end]=='[')
+		if(buffer[i] == '[') {
+			token[token_num].type = ARRAY;
+			token[token_num].start = i;
+			temp = arr;
+			for(end = i+1; ; end++) {
+				if(buffer[end] == '[')
 					temp++;
-				if(buffer[end]==']')
+				if(buffer[end] == ']')
 					temp--;
-				if(temp<0)
+				if(temp < 0)
 					break;
 			}
-			token[token_num].end=end;
+			token[token_num].end = end;
 			arr++;
+			size = 1;
+			for(int j = i; j < end; j++) {
+				if(buffer[j] == ':') size++;
+			}
+			token[token_num].size = size;
 			token_num++;
 		}
-		if(buffer[i]=='"'){
-			token[token_num].type=STRING;
-			token[token_num].start=i;
-			for(end=i+1;buffer[end]!='"';end++);
-			token[token_num].end=end;
-			i=end;
+		if(buffer[i] == '"') {
+			token[token_num].type = STRING;
+			token[token_num].start = i;
+			for(end = i+1; buffer[end] != '"'; end++);
+			token[token_num].end = end;
+			i = end;
 			str++;
+			size = 0;
+			if(buffer[end+1] == ':') size++;
+			token[token_num].size = size;
 			token_num++;
 		}
-		if((buffer[i]!='{')&&(buffer[i]!='}')&&(buffer[i]!='[')&&(buffer[i]!=']')&&(buffer[i]!='"')&&(buffer[i]!='\0')&&(buffer[i]!='\n')&&(buffer[i]!=' ')&&(buffer[i]!=':')&&(buffer[i]!=',')&&(buffer[i]!='\t')){
-			token[token_num].type=PRIMITIVE;
-			token[token_num].start=i;
-			for(end=i+1;buffer[end]==' ';end++);
-			token[token_num].end=end;
-			i=end;
+		if((buffer[i] != '{') && (buffer[i] != '}') && (buffer[i] != '[') && (buffer[i] != ']') && (buffer[i] != '"') && (buffer[i] != '\0') && (buffer[i] != '\n') && (buffer[i] != ' ') && (buffer[i] != ':') && (buffer[i] != ',') && (buffer[i] != '\t')) {
+			token[token_num].type = PRIMITIVE;
+			token[token_num].start = i;
+			for(end = i+1; buffer[end] == ' '; end++);
+			token[token_num].end = end;
+			i = end;
 			prm++;
+			size = 0;
+			if(buffer[end+1] == ':') size++;
+			token[token_num].size = size;
 			token_num++;
 		}
 	}
@@ -120,25 +135,24 @@ void tokenizer(jsmntok_t *token, char *buffer, int index){
 
 void print(jsmntok_t *token,int num,char*buffer){
 	int i,j;
-	for(i=0;i<num;i++){
-		printf("[%2d] ",i);
-		for(j=token[i].start;j<token[i].end+1;j++){
-			if(buffer[j]==EOF) continue;
-			printf("%c",buffer[j]);
+	for(i = 0;i < num; i++){
+		printf("[%2d] ", i);
+		for(j = token[i].start; j < token[i].end+1; j++){
+			if(buffer[j] == EOF) continue;
+			printf("%c", buffer[j]);
 		}
-		printf(" (size=0, %d~%d,",token[i].start,token[i].end);
-		if(token[i].type==1)
+		printf(" (size=%d, %d~%d,", token[i].size, token[i].start, token[i].end);
+		if(token[i].type == 1)
 			printf(" JSMN_OBJECT)\n");
-		else if(token[i].type==2)
+		else if(token[i].type == 2)
 			printf(" JSON_ARRAY)\n");
-		else if(token[i].type==3)
+		else if(token[i].type == 3)
 			printf(" JSON_STRING)\n");
-		else if(token[i].type==4)
+		else if(token[i].type == 4)
 			printf(" JSON_PRIMITIVE)\n");
 	}
 };
 
-//jsmntok_t *get_tokens(char *buffer) {
 int tokens(char *buffer) {
 	int count_tokens = 0;
 
@@ -155,14 +169,6 @@ int tokens(char *buffer) {
 	return count_tokens;
 };
 
-//	jsmntok_t *tokens = (jsmntok_t *)malloc(sizeof(jsmntok_t) * 128);
-//	
-//	if(buffer[0] = '{' && buffer[strlen(buffer-1)] == '}') {
-//		tokens[0]->type = OBJECT;
-//		tokens[0]->start = 0;
-//		tokens[0]->end = strlen(buffer)-1;
-//	}
-	
 /* 1. so far we just counted the number of tokens (not complete, because numbers aren't read)
  * 2. we have to tokenize:
  *   1) by objects
